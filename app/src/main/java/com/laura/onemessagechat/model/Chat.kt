84e4d41872cbdb85bdc025983953a664cd18d4d1
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-11-28 19:49:00 -0300

    feat: cria classe de chat

---
+++ app/src/main/java/com/laura/onemessagechat/model/Chat.kt
@@ -0,0 +1,6 @@
+package com.laura.onemessagechat.model
+
+class Chat {
+    var id: Int = -1
+    var message: String = ""
+}
--- app/src/main/java/com/laura/onemessagechat/view/MainActivity.kt
+++ app/src/main/java/com/laura/onemessagechat/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.laura.onemessagechat
+package com.laura.onemessagechat.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.laura.onemessagechat.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
