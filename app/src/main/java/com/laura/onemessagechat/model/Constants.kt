ae5e767514a687110e1f77f29296675dacd16fe0
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-12-02 20:00:54 -0300

    feat: cria constantes e handler

---
+++ app/src/main/java/com/laura/onemessagechat/model/Constants.kt
@@ -0,0 +1,7 @@
+package com.laura.onemessagechat.model
+object Constants {
+    const val EXTRA_CHAT = "EXTRA_CHAT"
+    const val VIEW_CHAT = "VIEW_CHAT"
+    const val INVALID_CHAT_ID = -1
+    const val CHAT_ARRAY = "CHAT_ARRAY"
+}
--- app/src/main/java/com/laura/onemessagechat/view/MainActivity.kt
+++ app/src/main/java/com/laura/onemessagechat/view/MainActivity.kt
@@ -2,9 +2,38 @@ package com.laura.onemessagechat.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import com.laura.onemessagechat.R
+import com.laura.onemessagechat.databinding.ActivityMainBinding
+import com.laura.onemessagechat.model.Chat
+import com.laura.onemessagechat.model.Constants.CHAT_ARRAY
 
 class MainActivity : AppCompatActivity() {
+    //ViewBinding
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    //Data Source
+    private val chatList: MutableList<Chat> = mutableListOf()
+
+    val updateChatListHandler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            msg.data.getParcelableArray(CHAT_ARRAY)?.also { chatArray ->
+
+                chatList.clear()
+                chatArray.forEach {
+                    chatList.add(it as Chat)
+                }
+
+//                contactAdapter.notifyDataSetChanged()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
