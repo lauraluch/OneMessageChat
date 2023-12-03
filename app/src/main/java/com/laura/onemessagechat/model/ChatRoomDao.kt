5bbed723b738261c79fd703320e45d06196bec12
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-12-02 22:58:56 -0300

    feat: cria room dao de conversa

---
+++ app/src/main/java/com/laura/onemessagechat/model/ChatRoomDao.kt
@@ -0,0 +1,32 @@
+package com.laura.onemessagechat.model
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface ChatRoomDao {
+
+    companion object {
+        const val CHAT_DATABASE_FILE = "chats_room"
+        private const val CHAT_TABLE = "chat"
+        private const val ID_COLUMN = "id"
+        private const val MESSAGE_COLUMN = "message"
+
+    }
+
+    @Insert
+    fun createChat(chat: Chat)
+
+    @Query("SELECT *FROM $CHAT_TABLE WHERE $ID_COLUMN = :id")
+    fun readChat(id: Int): Chat?
+    
+    @Query("SELECT * FROM $CHAT_TABLE ORDER BY $MESSAGE_COLUMN")
+    fun readAllChats(): MutableList<Chat>
+
+    @Update
+    fun updateChat(contact: Chat): Int
+
+}
