7f572cb7033f78a5bd06298244ad1a759b75c403
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-12-03 19:06:37 -0300

    feat: cria funções da tela do chat e altera xml

--- app/src/main/java/com/laura/onemessagechat/view/ChatActivity.kt
+++ app/src/main/java/com/laura/onemessagechat/view/ChatActivity.kt
@@ -1,4 +1,56 @@
 package com.laura.onemessagechat.view
 
-class ChatActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.laura.onemessagechat.databinding.ChatActivityBinding
+import com.laura.onemessagechat.model.Chat
+import com.laura.onemessagechat.model.Constants.EXTRA_CHAT
+import com.laura.onemessagechat.model.Constants.VIEW_CHAT
+import kotlin.random.Random
+
+class ChatActivity : AppCompatActivity() {
+    private val cab: ChatActivityBinding by lazy {
+        ChatActivityBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(cab.root)
+
+        setSupportActionBar(cab.toolbarIn.toolbar)
+        supportActionBar?.subtitle = "Chat details"
+
+        val receivedChat = intent.getParcelableExtra<Chat>(EXTRA_CHAT)
+        receivedChat?.let {_receivedChat ->
+            val viewChat: Boolean = intent.getBooleanExtra(VIEW_CHAT, false)
+            with(cab) {
+                if (viewChat) {
+                    messageEt.isEnabled = false
+                    saveBt.visibility = View.GONE
+                }
+                messageEt.setText(_receivedChat.message)
+
+            }
+
+
+        }
+
+        with(cab) {
+            saveBt.setOnClickListener {
+                val chat = Chat(
+                    id = receivedChat?.id?: generateId(),
+                    message = messageEt.text.toString()
+                )
+
+                val resultIntent = Intent()
+                resultIntent.putExtra(EXTRA_CHAT, chat)
+                setResult(RESULT_OK, resultIntent)
+                finish()
+            }
+        }
+    }
+
+    private fun generateId() = Random(System.currentTimeMillis()).nextInt()
 }
